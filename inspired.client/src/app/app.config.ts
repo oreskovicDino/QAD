0aba1d986f92c0b4a3a347d9893cf8e81066eef7
Dino Orešković <oreskovic.dino@gmail.com>
2025-01-02 18:16:15 +0100

    FB config removed

--- inspired.client/src/app/app.config.ts
+++ inspired.client/src/app/app.config.ts
@@ -7,21 +7,11 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { urlInterceptor } from './core/interceptors/url.interceptor';
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBL4brdTw07Th6cByK0bXoCdEhYdhuEVjA",
-  authDomain: "inspiredauthapp.firebaseapp.com",
-  projectId: "inspiredauthapp",
-  storageBucket: "inspiredauthapp.firebasestorage.app",
-  messagingSenderId: "316221502229",
-  appId: "1:316221502229:web:8941453d24f8b836ce6f1c",
-  measurementId: "G-7C1Z3YTMHQ"
-};
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideHttpClient(withInterceptors([authInterceptor, urlInterceptor])),
     provideZoneChangeDetection({ eventCoalescing: true }),
